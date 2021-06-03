b8306b00372c6892e75aad7488ba98be6b785528
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-02 21:34:42 -0500

    Refactored sayHello function

--- code.js
+++ code.js
@@ -4,12 +4,9 @@ function helloWorld() {
 }
 
 function sayHello(name) {
-    if (typeof name === 'undefined' || typeof name === 'boolean' || typeof name == 'number') {
-        name = "World";
-    } else if (name == null || name === '' || !isNaN(parseFloat(name))) {
-        name = "World";
-    } else if (Array.isArray(name) || typeof name == 'object' || typeof name == 'function') {
+    if (typeof name != 'string' || name === '' || !isNaN(parseFloat(name))) {
         name = "World";
     }
+
     return "Hello, " + name + "!";
 }
